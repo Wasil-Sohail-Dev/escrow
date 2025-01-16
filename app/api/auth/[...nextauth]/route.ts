0909acdf1dbf4443304369789e07fa487479af06
Wasil-Sohail-Dev <wasilsohaildev@paklogics.com>
2025-01-17 00:05:49 +0500

    finalized

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,7 +0,0 @@
-// /pages/api/auth/[...nextauth]/route.ts
-import NextAuth from "next-auth";
-import { authOptions } from "./options";
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST }
